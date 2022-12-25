ce1c8aa64e747927a6ad2bdf3f049f89cb1e5a21
Adesh Mahure <117083424+Adesh-Mahure@users.noreply.github.com>
2022-12-26 00:16:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "adesh-first-bucket-for-terraform-project"
+    bucket = "bucket-for-terra-project-adii"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamo-db-table-first"
